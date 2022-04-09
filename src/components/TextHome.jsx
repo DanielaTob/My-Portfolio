78cbc2837d8020ff12ebcb13067932c2ee469b3a
daniela <moreno.tobar.daniela@gmail.com>
2022-04-09 14:45:47 -0500

    font

--- src/components/TextHome.jsx
+++ src/components/TextHome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TextHome() {
   return (
     <div>
-        <div className='font-paragraph text-2xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-8xl pl-9 pt-5 text-colortitle font-bold'>
+        <div className='font-paragraph text-2xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-7xl pl-9 pt-5 text-colortitle font-bold'>
             <h1>I’m Daniela, </h1>
             <p>A Full Stack developer</p>
             <p>and Problem Solver</p>
