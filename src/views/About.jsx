b551d1eb4f37405bae2e652f91ab10b5ad079941
daniela <moreno.tobar.daniela@gmail.com>
2022-04-10 16:08:36 -0500

    img

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -4,7 +4,7 @@ import TextAbout from "../components/TextAbout";
 
 export default function About() {
   return (
-    <div className="sm:pr-9 pb-40 bg-bgdark grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-flow-col auto-cols-max gap-2">
+    <div className="sm:pr-9 pb-30 bg-bgdark grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-flow-col auto-cols-max gap-2">
         <TextAbout />
         <AnimationTwo />
     </div>
