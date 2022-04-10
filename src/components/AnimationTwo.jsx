ee96a547fe2ee1a300cac66cf28c5ae00892be7e
daniela <moreno.tobar.daniela@gmail.com>
2022-04-10 15:57:51 -0500

    img2

---
+++ src/components/AnimationTwo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import imgtwo from '../assets/illustration2.png'
+
+export default function AnimationTwo() {
+  return (
+    <div>
+        <div>
+        <img className='' src={imgtwo} alt="img1" />
+        </div>
+    </div>
+  )
+}
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import AnimationTwo from "../components/AnimationTwo";
 import TextAbout from "../components/TextAbout";
 
 export default function About() {
   return (
     <div className="sm:pr-9 pb-40 bg-bgdark grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-flow-col auto-cols-max gap-2">
         <TextAbout />
+        <AnimationTwo />
     </div>
   );
 }
