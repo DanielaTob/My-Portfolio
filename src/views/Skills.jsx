69c5c973b51dc0ccc626d98f24567fbdcfd25547
daniela <moreno.tobar.daniela@gmail.com>
2022-04-10 22:07:07 -0500

    skills done

--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -4,6 +4,17 @@ import { IoLogoJavascript } from 'react-icons/io';
 import { DiMongodb} from 'react-icons/di';
 import { FaNodeJs } from 'react-icons/fa';
 import { SiExpress } from 'react-icons/si';
+import { AiFillHtml5 } from 'react-icons/ai';
+import { DiCss3} from 'react-icons/di';
+import { SiTailwindcss} from 'react-icons/si';
+import { SiBootstrap} from 'react-icons/si';
+import { SiMysql} from 'react-icons/si';
+import { DiGit} from 'react-icons/di';
+import { AiFillGithub } from 'react-icons/ai';
+import { CgFigma} from 'react-icons/cg';
+import { SiAdobeillustrator } from 'react-icons/si';
+import { SiAdobephotoshop} from 'react-icons/si';
+
 
 const Skills = () => {
   return (
@@ -11,40 +22,91 @@ const Skills = () => {
         <div className="font-paragraph text-colortitle font-bold pl-9 pt-16 text-3xl">
           <h1>My Skills</h1>
 
-          <div className='wrapper grid grid-cols-5 pt-16 text-red'>
+          <div className='wrapper grid grid-cols-5 grid-rows-4 gap-10 pt-20 text-red'>
 
             
-          <div class="icon program animation">
-            <span class="tooltip">ReactJs</span>
+          <div className="icon program animation">
+            <span className="tooltip font-paragraph">ReactJs</span>
             <span><i><RiReactjsLine className="text-2xl"/> </i></span>
           </div>
 
-          <div class="icon program animation">
-            <span class="tooltip">JavasScript</span>
+          <div className="icon program animation">
+            <span className="tooltip">JavasScript</span>
             <span><i><IoLogoJavascript className="text-2xl"/> </i></span>
           </div>
 
 
-          <div class="icon program animation">
-            <span class="tooltip">MongoDB</span>
+          <div className="icon program animation">
+            <span className="tooltip">MongoDB</span>
             <span><i><DiMongodb className="text-2xl"/> </i></span>
           </div>
 
 
-          <div class="icon program animation">
-            <span class="tooltip">NodeJS</span>
+          <div className="icon program animation">
+            <span className="tooltip">NodeJS</span>
             <span><i><FaNodeJs  className="text-2xl"/> </i></span>
           </div>
 
 
-          <div class="icon program animation">
-            <span class="tooltip">Express</span>
+          <div className="icon program animation">
+            <span className="tooltip">Express</span>
             <span><i><SiExpress  className="text-2xl"/> </i></span>
           </div>
 
-           
 
 
+          
+          <div className="icon program animation">
+            <span className="tooltip">HTML</span>
+            <span><i><AiFillHtml5  className="text-2xl"/> </i></span>
+          </div>
+
+          <div className="icon program animation">
+            <span className="tooltip">CSS</span>
+            <span><i><DiCss3  className="text-2xl"/> </i></span>
+          </div>
+
+          <div className="icon program animation">
+            <span className="tooltip">TailwindCss</span>
+            <span><i><SiTailwindcss  className="text-2xl"/> </i></span>
+          </div>
+
+
+          <div className="icon program animation">
+            <span className="tooltip">Bootstrap</span>
+            <span><i><SiBootstrap  className="text-2xl"/> </i></span>
+          </div>
+
+        
+          <div className="icon program animation">
+            <span className="tooltip">Mysql</span>
+            <span><i><SiMysql className="text-2xl"/> </i></span>
+          </div>
+
+          <div className="icon program animation">
+            <span className="tooltip">Git</span>
+            <span><i><DiGit className="text-2xl"/> </i></span>
+          </div>
+
+          <div className="icon program animation">
+            <span className="tooltip">GitHub</span>
+            <span><i><AiFillGithub  className="text-2xl"/> </i></span>
+          </div>
+
+          <div className="icon program animation">
+            <span className="tooltip">Figma</span>
+            <span><i><CgFigma className="text-2xl"/> </i></span>
+          </div>
+
+          <div className="icon program animation">
+            <span className="tooltip">Illustrator</span>
+            <span><i><SiAdobeillustrator className="text-2xl"/> </i></span>
+          </div>
+
+          <div className="icon program animation">
+            <span className="tooltip">Photoshop</span>
+            <span><i><SiAdobephotoshop className="text-2xl"/> </i></span>
+          </div>
 
           </div>
         </div>
