82f9caf2ff30e384bd125bbb972df7d8cd51edc6
daniela <moreno.tobar.daniela@gmail.com>
2022-04-12 15:02:16 -0500

    routes

--- src/components/buttons/GroupButton.jsx
+++ src/components/buttons/GroupButton.jsx
@@ -5,9 +5,9 @@ export default function GroupButton() {
   return (
     <div>
       <div className="flex gap-4 pl-9 pt-10">
-        <button className="bg-red gap-2 text-white font-fontFamily-paragraph  font-semibold py-2 px-4  rounded-lg inline-flex items-center">
+        <a href="#projects" className="bg-red gap-2 text-white font-fontFamily-paragraph  font-semibold py-2 px-4  rounded-lg inline-flex items-center">
          <span>View My Projects</span> <IoIosArrowDroprightCircle className="fill-current w-4 h-4 mr-2"/>
-        </button>
+        </a>
         <div>
 
 
