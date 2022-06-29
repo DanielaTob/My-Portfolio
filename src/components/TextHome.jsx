7fd524fa7554d1dda642c95baed577fe5a9e3c0e
daniela <moreno.tobar.daniela@gmail.com>
2022-06-29 13:44:25 -0500

    changes

--- src/components/TextHome.jsx
+++ src/components/TextHome.jsx
@@ -6,7 +6,7 @@ export default function TextHome() {
     <div>
         <div className='animate__animated animate__fadeInLeft font-paragraph text-2xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-6xl pl-9 pt-5 text-colortitle font-bold'>
             <h1>I’m Daniela, </h1>
-            <p>A Front End developer</p>
+            <p>Full Stack developer</p>
             <p>and Problem Solver</p>
         </div>
     </div>
--- src/components/buttons/ButtonCv.jsx
+++ src/components/buttons/ButtonCv.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ButtonCv() {
   return (
     <div className='pl-9 pt-9'>
-        <a href='https://drive.google.com/file/d/1OZUWC1vOKN2Zf0tYtEgEX_TK0otMD3my/view?usp=sharing' target="_blank">
+        <a href='https://drive.google.com/file/d/1ye98mxgWYnLRxiwyee0HjiSOuyzN01DI/view?usp=sharing' target="_blank">
         <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 bg-transparent hover:bg-red text-red font-semibold font-fontFamily-paragraph hover:text-white py-2 px-20 border border-red hover:border-transparent rounded-lg">
             CV
         </button>
