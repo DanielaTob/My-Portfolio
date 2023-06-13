c3020efab54be48e2ac237dc52441edb51eaeb57
daniela <moreno.tobar.daniela@gmail.com>
2023-06-13 11:11:28 -0500

    new changes

--- src/components/TextAbout.jsx
+++ src/components/TextAbout.jsx
@@ -7,7 +7,7 @@ export default function TextAbout() {
     <h1 className="font-paragraph text-colortitle font-bold text-center lg:pt-8 md:pt-8 pt-8 sm:pt-16 text-3xl animate__animated animate__fadeInLeft">A Little Bit About Me</h1>
     <div className='text-center mx-4 leading-8 font-title sm:text-base md:text-lg lg:text-xl xl:text-xl 2xl:text-xl text-colorparagraph pt-10 px-10'>
         <p className='pb-2 text-justify inline-block'>Hello again! Thanks for scrolling this far!! 🤗 </p>
-        <p className='pb-2 text-justify inline-block'>I am a web developer with experience in projects with a focus on responsive design and accessibility, especially with scrum methodology. Creative expression is what lights me up, and I'm always open to learning about new opportunities. </p> 
+        <p className='pb-2 text-justify inline-block'>I am a Software developer with experience in projects with a focus on responsive design and accessibility, especially with scrum methodology. Creative expression is what lights me up, and I'm always open to learning about new opportunities. </p> 
         <p className='text-justify inline-block '>I'm passionate about coding and problem solving through code, and I'm excited to work alongside other amazing programmers and learn so much more!</p>
     </div>
     </div>
